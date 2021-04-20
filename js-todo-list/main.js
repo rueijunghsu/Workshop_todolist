e352cd9124603d4801b6c453a5bb494687c68b30
Ray Hsu <ray199185@gmail.com>
2021-04-20 16:00:48 +0800

    1-3 task done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,50 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  
+  //選取
+  document.querySelectorAll('li').forEach(function(item){
+    item.addEventListener('click', function(e){
+      e.target.classList.toggle('checked');
+      //this.classList.toggle('checked');
+    });
+  })
+  //刪除
+  document.querySelectorAll('.close').forEach(function(item){
+    item.addEventListener('click', function(e){
+      e.target.parentElement.remove()
+      //選span-close的上一層
+    });
+  })
+
+  //增加???
+  document.getElementById('addBtn').addEventListener('click',function(){
+    let newin = document.querySelector('input').value;
+
+    document.querySelector('ul').appendChild(addli(newin));
+    
+    //清空輸入欄
+    
+    document.querySelector('input').value ='';
+
+  })
+
+  function addli(newinput){
+    let newli = document.createElement('li');
+    let newspan = document.createElement('span');
+    newli.textContent = newinput;
+
+    newspan.classList.add('close');
+    newspan.textContent = 'x';
+    newli.appendChild(newspan);
+
+    //出生的時候就有事件
+    newli.addEventListener('click', function(e){
+      e.target.classList.toggle('checked');
+    })
+    newspan.addEventListener('click', function(e){
+      e.target.parentElement.remove();
+    })
+    
+    return newli;
+  }
+})
